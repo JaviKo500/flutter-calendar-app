202b8dcb5a8224c551bc5812373d3c8dc08e9545
JaViKo500 <javikogutierrez64@gmail.com>
2025-07-24 16:47:54 -0500

    feat: add marker calendar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -13,6 +13,31 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   DateTime _selectedDate = DateTime.now();
   CalendarFormat _calendarFormat = CalendarFormat.month;
+  List< Event > _events = [
+    Event(title: 'Event 1', date: DateTime.utc(2025, 7, 16)),
+    Event(title: 'Event 2', date: DateTime.utc(2025, 7, 17)),
+    Event(title: 'Event 3', date: DateTime.utc(2025, 7, 18)),
+    Event(title: 'Event 4', date: DateTime.utc(2025, 7, 19)),
+    Event(title: 'Event 5', date: DateTime.utc(2025, 7, 20)),
+  ];
+
+  List<Event> _getEventsForDay(DateTime day) {
+    List<Event> eventsForDay = <Event>[];
+    for (Event event in _events) {
+
+      final DateTime emitDate = event.date;
+      
+      if (isSameDay(day, emitDate)) {
+        eventsForDay.add(Event(
+          title: event.title,
+          date: emitDate,
+        ));
+      }
+      print(isSameDay(day, emitDate));
+    }
+    print('eventsForDay: ${eventsForDay.length}');
+    return eventsForDay;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,25 +47,25 @@ class _CalendarPageState extends State<CalendarPage> {
       ),
       body: Column(
         children: [
-          TableCalendar(
+          TableCalendar<Event>(
             firstDay: DateTime.utc(2010, 10, 16),
             lastDay: DateTime.utc(2030, 3, 14),
             focusedDay: _selectedDate,
             currentDay: _selectedDate,
-            eventLoader: (day) {
-              print('eventLoader day: $day');
-              return [
-                DateTime(day.year, day.month, day.day, 12, 0),
-              ];
-            },
+            eventLoader: _getEventsForDay,
             onDaySelected: (selectedDay, focusedDay) {
               print('selectedDay: $selectedDay, focusedDay: $focusedDay');
+              if (!isSameDay(_selectedDate, selectedDay)) {
+                setState(() {
+                  _selectedDate = selectedDay;
+                });
+              }
               setState(() {
                 _selectedDate = selectedDay;
               });
             },
             onPageChanged: (focusedDay) {
-              print('onPageChanged focusedDay: $focusedDay');
+              print('focusedDay: $focusedDay');
             },
             calendarFormat: _calendarFormat,
             onFormatChanged: (format) {
@@ -48,9 +73,40 @@ class _CalendarPageState extends State<CalendarPage> {
                 _calendarFormat = format;
               });
             },
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, day, events) {
+                if (_getEventsForDay(day).isEmpty) return null;
+                return Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          shape: BoxShape.circle,
+                        ),
+                        width:16.0,
+                        height: 16.0,
+                        alignment: Alignment.center,
+                        child: Text('${_getEventsForDay(day).length}', style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                        ), ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
           )
         ],
       ),
     );
   }
+}
+class Event {
+  final String title;
+  final DateTime date;
+
+  Event({required this.title, required this.date});
 }
